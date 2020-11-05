0a3ad1f7f9900dffdf196e26a70f5765ca68f754
Helen Gualli <helen.gualli.7e4@itb.cat>
2020-11-05 12:00:42 +0100

    hem afegit If.java

---
+++ If.java
@@ -0,0 +1,11 @@
+public class If{
+	public static void main(String[] args){
+		boolean a = true ;
+		if (a==true){
+			System.out.println("Hola");
+
+		}else{
+			System.out.println("Adeu");
+		}
+	}
+}
